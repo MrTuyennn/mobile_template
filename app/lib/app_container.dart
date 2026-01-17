76ee9ef06a256d148d679b0ddb55897982b1c08b
juncook <nguyenngoctuyen188@gmail.com>
2026-01-17 14:51:33 +0700

    #refactor: test loading useState

--- app/lib/app_container.dart
+++ app/lib/app_container.dart
@@ -1,5 +1,7 @@
 import 'package:app/routers/auth_route_guard.dart';
 import 'package:app/routers/router.dart';
+import 'package:app_logger/logger.dart';
+import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,6 +12,21 @@ class AppContainer extends HookConsumerWidget {
   const AppContainer({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final loading = useLoadingState();
+
+    Future<String> loadingState() async {
+      return await loading.whileLoading(() async {
+        logger.d('handle data');
+        if (!context.mounted) return 'dispose';
+        return 'done';
+      });
+    }
+
+    useEffect(() {
+      loadingState();
+      return null;
+    }, []);
+
     final router = useMemoized(
       () => AppRouter(authRouteGuard: AuthRouteGuard(ref: ref)),
     );
