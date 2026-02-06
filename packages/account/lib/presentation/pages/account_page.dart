953dd276f7a6164a3a3b8f66e3c290c33bec3bb5
juncook <nguyenngoctuyen188@gmail.com>
2026-02-06 18:06:55 +0700

    #feat: add device info

--- packages/account/lib/presentation/pages/account_page.dart
+++ packages/account/lib/presentation/pages/account_page.dart
@@ -1,4 +1,6 @@
+import 'package:app_logger/logger.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:device_info_app/device_info_app.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -10,11 +12,23 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends State<AccountPage> {
+  Future<void> _getAppInfo() async {
+    final deviceInfo = await DeviceInfoApp.getDeviceInfo();
+    logger.e(deviceInfo!.toJson());
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getAppInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.redAccent,
-      child: Center(child: Text('Account')),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Account Page')),
+      body: const Center(child: Text('Welcome to the Account Page!')),
     );
   }
 }
