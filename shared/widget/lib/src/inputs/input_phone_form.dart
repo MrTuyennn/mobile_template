c8abd32f43a0d31f38b1a2512d994b6f9adea00d
juncook <nguyenngoctuyen188@gmail.com>
2026-02-21 10:20:26 +0700

    #feat: implement example input phone form

---
+++ shared/widget/lib/src/inputs/input_phone_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InputPhoneForm extends StatefulWidget {
+  const InputPhoneForm({super.key});
+
+  @override
+  State<InputPhoneForm> createState() => _InputPhoneFormState();
+}
+
+class _InputPhoneFormState extends State<InputPhoneForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
